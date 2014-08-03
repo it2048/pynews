66dd3b7ccb0621671b78b40ea58a38b14375678c
jsjscool <jsjscool@163.com>
2014-08-04 00:50:10 +0800

    第一个版本

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 <a href="http://it2048.cn/">My blog</a>
-这个项目用来封装一个网络爬虫类。
+
+用python写的一个迭代获取谷歌最快IP的脚本;
+ip0.txt 存放着谷歌全球4000++IP;
+index.py文件可检测ip速度。
--- index.py
+++ index.py
@@ -1,9 +1,46 @@
-__author__ = 'xiongfanglei'
-import requests
-import bs4
-response = requests.get('http://it2048.cn/')
-soup = bs4.BeautifulSoup(response.text)
-links = soup.select()
+__author__ = 'it2048'
+
+import urllib2
+import socket
+import re
+import time
+#set default timeout
+socket.setdefaulttimeout(1)
 
 
-print(response.text)
+count = 0
+#Iteration 5 times,you can modify
+while(count<5):
+    ct1 = str(count)
+    #ip0.txt file use to store google ip
+    file_object = open('ip'+ct1+'.txt')
+    count = count+1
+    ct2 = str(count);
+    fobj = open('ip'+ct2+'.txt','a')
+    fobj.truncate()
+    try:
+         all_the_text = file_object.read()
+    finally:
+         file_object.close( )
+    arr = all_the_text.split(',')
+    rege = ur"([0-9]{1,3}\.){3}[0-9]{1,3}"
+    for ip in arr:
+        if re.match(rege, ip):
+            try:
+                start = time.clock()
+                fp = urllib2.urlopen("http://"+ip,timeout=1)
+                end = time.clock()
+                fobj.write(ip+",")
+                print "%s %f" %(ip,end-start)
+            except Exception, e:
+                continue
+    fobj.close()
+
+
+
+
+
+
+
+
+
